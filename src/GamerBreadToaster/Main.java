42982c3579b79a5f5f7928cabfbeb29c3144bafb
GamerBreadToaster <goedemoorgen@gmail.com>
2025-06-05 11:16:53 +0200

    Added operationFinder method

--- src/GamerBreadToaster/Main.java
+++ src/GamerBreadToaster/Main.java
@@ -1,12 +1,24 @@
 package GamerBreadToaster;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     // easy print method
     static void print(Object message) {System.out.println(message);}
+
+    // operations finder
+    public static Map<Integer, String> operationFinder(String[] input) {
+        // variables
+        String[] operations = {"*", "/", "+", "-", "^"};
+        Map<Integer, String> operationMap = new HashMap<>();
+
+        // finding the operation in the input
+        for (int x = 0; x < input.length; x++) {for (String y: operations) {
+            if (input[x].equals(y)) operationMap.put(x, y);
+        }}
+
+        return operationMap;
+    }
 
     public static void main(String[] args) {
         /* EXERCISE 1: Easy Calculator
@@ -37,51 +49,37 @@ public class Main {
             String operation = "";
             boolean exit = false;
             String[] inputParsed;
+            String input = "";
+            List<Integer> keys = new ArrayList<>();
             double result = 0;
-            double number1 = 0;
-            double number2 = 0;
+            double[] number;
+            Map<Integer, String> operationMap;
 
             // input
             print("Enter your formula as for example '6*9' and '6 * 9'.\nYou can use the '+', '-', '*', '/' and '^' operators\nYou can use 'ans' to reuse last answer:");
             while (!exit) {
-                inputParsed = scanner.nextLine().replaceAll(" ", "").split("(?<=[-+*/^])|(?=[-+*/^])");
+                input = scanner.nextLine();
+                inputParsed = input.replaceAll(" ", "").split("(?<=[-+*/^])|(?=[-+*/^])");
 
-                // Awner memory
-                if (inputParsed[0].equals("ans")) {
-                    inputParsed[0] = Double.toString(awnser);
-                } else if (inputParsed[2].equals("ans")) {
-                    inputParsed[2] = Double.toString(awnser);
+                // operations parsing
+                operationMap = operationFinder(inputParsed);
+                for (Map.Entry<Integer, String> entry: operationMap.entrySet()) {
+                    keys.add(entry.getKey());
                 }
-
-                // will make sure number 1 and 2 are valid numbers
-                try {
-                    number1 = Double.parseDouble(inputParsed[0]);
-                    number2 = Double.parseDouble(inputParsed[2]);
-                } catch (NumberFormatException e) {
-                    print("Error: Not a valid number: " + e);
-                    continue;
-                }
-
-                // divide by zero check
-                if (number2 == 0 && operation.equals("/")) {
-                    print("Error: Can't divide by 0");
-                    continue;
-                }
-
-                // operations check
-                operation = inputParsed[1];
-                for (String operator : operations) {
-                    if (operator.equals(operation)) {
-                        exit = true;
-                        break;
+                // two operators next to each other checker
+                for (int key = 0; key < keys.size();) {
+                    int key2 = key+1;
+                    if (key2 < keys.size()) {
+                        if ((keys.get(key2) - keys.get(key)) != 2) {
+                            print("Error: You can't have two operators next to each other");
+                            continue;
+                        }
                     }
-                }
-                if (!exit) {
-                    print("Error: unsupported operator: " + operation);
                 }
             }
 
             // result
+            /*
             result = switch (operation) {
                 case "+" -> number1 + number2;
                 case "-" -> number1 - number2;
@@ -93,21 +91,8 @@ public class Main {
             awnser = result;
 
             // exit
-            print(number1 + " " + operation + " " + number2 + " = " + result);
+            print(input + " = " + result); */
             print("Do you want to calculate again? Y or N:");
         } while (!scanner.nextLine().equalsIgnoreCase("n"));
-    }
-
-    // operations finder
-    public Map<Integer, String> operationFinder(String[] input) {
-        String[] operations = {"*", "/", "+", "-", "^"};
-        Map<Integer, String> operationMap = new HashMap<>();
-
-        // finding the operation in the input
-        for (int x = 0; x < input.length; x++) {for (String y: operations) {
-            if (input[x].equals(y)) operationMap.put(x, y);
-        }}
-
-        return operationMap;
     }
 }
