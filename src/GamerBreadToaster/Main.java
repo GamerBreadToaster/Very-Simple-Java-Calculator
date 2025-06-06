f1c189303d49ac8b68d060ac4daf68fdfe4d6f5d
GamerBreadToaster <goedemoorgen@gmail.com>
2025-06-06 09:35:08 +0200

    Added operationFinder method

--- src/GamerBreadToaster/Main.java
+++ src/GamerBreadToaster/Main.java
@@ -25,9 +25,10 @@ public class Main {
 
         // scanner and variables
         Scanner scanner = new Scanner(System.in);
-        do {
+        while (true) {
             // variables
             boolean exit = false;
+            boolean again = false;
             String[] inputParsed = null;
             String inputRaw = "";
             List<Integer> keys = new ArrayList<>();
@@ -36,6 +37,7 @@ public class Main {
 
             // input
             print("Enter your formula as for example '6*9' and '6 * 9'.\nYou can use the '+', '-', '*', '/' and '^' operators\nYou can use 'ans' to re-use last answer:");
+
             while (!exit) {
                 inputRaw = scanner.nextLine();
                 inputParsed = inputRaw.replaceAll(" ", "").split("(?<=[-+*/^])|(?=[-+*/^])");
@@ -51,51 +53,73 @@ public class Main {
                     if (key2 < keys.size()) {
                         if ((keys.get(key2) - keys.get(key)) != 2) {
                             print("Error: You can't have two operators next to each other");
-                            break;}}
+                            again = false;
+                            break;
+                        } else {exit = true;}
+                    }
+                    if (again) {continue;}
                 }
-                exit = true;
             }
             List<String> inputList = new ArrayList<>(Arrays.asList(inputParsed));
 
             // result
             // calculation rules: power -> division or multiplication  -> sum / subtraction
+            again = false;
+            boolean divisionBy0 = false;
             while (inputList.size() != 1) {
-                Map.Entry<Integer, String> entry2 = null;
                 for (Map.Entry<Integer, String> entry : operationMap.entrySet()) {
-                    entry2 = entry;
-                    double number1 = Double.parseDouble(inputList.get(entry.getKey() - 1));
-                    double number2 = Double.parseDouble(inputList.get(entry.getKey() + 1));
+                    // to the power of
                     if (entry.getValue().equals("^")) {
+                        double number1 = Double.parseDouble(inputList.get(entry.getKey() - 1));
+                        double number2 = Double.parseDouble(inputList.get(entry.getKey() + 1));
                         result = pow(number1, number2);
+                        operationMap = parseList(entry, inputList, result);
+                        again = true;
                         break;
-                    } else if (entry.getValue().equals("*") | entry.getValue().equals("/")) {
+                    }
+                }
+                if (again) {again = false; continue;} // these are to make sure the parsing doesn't affect the order of the Map, otherwise 6+6*9*2 would come out as 120 because of wrong order.
+                for (Map.Entry<Integer, String> entry : operationMap.entrySet()) {
+                    // Multiply x by y or division of x by y
+                    if (entry.getValue().equals("*") || entry.getValue().equals("/")) {
+                        double number1 = Double.parseDouble(inputList.get(entry.getKey() - 1));
+                        double number2 = Double.parseDouble(inputList.get(entry.getKey() + 1));
                         // dubbel if statement to make sure the multi doesn't come first even if division is first.
                         if (entry.getValue().equals("*")) {
                             result = multi(number1, number2);
                         } else {
+                            if (number2 == 0) {divisionBy0 = true; break;}
                             result = div(number1, number2);
                         }
-                        break;
-                    } else if (entry.getValue().equals("+") | entry.getValue().equals("-")) {
-                        // dubbel if statement to make sure the + doesn't come first even if - is first.
-                        if (entry.getValue().equals("+")) {
-                            result = sum(number1, number2);
-                        } else {result = sub(number1, number2);}
+                        operationMap = parseList(entry, inputList, result);
+                        again = true;
                         break;
                     }
                 }
-                // removing from old numbers and operator from list and replacing it with the result
-                assert entry2 != null;
-                inputList.remove(entry2.getKey() + 1);
-                inputList.remove(entry2.getKey().intValue());
-                inputList.remove(entry2.getKey() - 1);
-                inputList.add(entry2.getKey() - 1, String.valueOf(result));
-                operationMap = operationFinder(inputList.toArray());
+                if (divisionBy0) {print("Error: can't divide by 0"); result = 0; break;}
+                if (again) {again = false; continue;}
+                for (Map.Entry<Integer, String> entry : operationMap.entrySet()) {
+                    // x + y or x - y
+                    if (entry.getValue().equals("+") || entry.getValue().equals("-")) {
+                        double number1 = Double.parseDouble(inputList.get(entry.getKey() - 1));
+                        double number2 = Double.parseDouble(inputList.get(entry.getKey() + 1));
+                        // dubbel if statement to make sure the + doesn't come first even if - is first.
+                        if (entry.getValue().equals("+")) {
+                            result = sum(number1, number2);
+                        } else {
+                            result = sub(number1, number2);
+                        }
+                        operationMap = parseList(entry, inputList, result);
+                    }
+                }
             }
             // exit
-            print(inputRaw + " = " + result);
-            print("Do you want to calculate again? Y or N:");
-        } while (!scanner.nextLine().equalsIgnoreCase("n"));
+            if (!divisionBy0) {
+                print(inputRaw + " = " + result);
+                print("Do you want to calculate again? Y or N:");
+                if (scanner.nextLine().equalsIgnoreCase("n")) {break;}
+            }
+        }
     }
 
     // easy print method
@@ -105,7 +129,7 @@ public class Main {
     public static Map<Integer, String> operationFinder(Object[] input) {
         // variables
         String[] operations = {"*", "/", "+", "-", "^"};
-        Map<Integer, String> operationMap = new HashMap<>();
+        Map<Integer, String> operationMap = new LinkedHashMap<>();
 
         // finding the operation in the input
         for (int x = 0; x < input.length; x++) {for (String y: operations) {
@@ -113,6 +137,16 @@ public class Main {
         }}
 
         return operationMap;
+    }
+
+    // this makes sure that operationMap is properly parsed so
+    public static Map<Integer, String> parseList(Map.Entry<Integer, String> entry, List<String> inputList, Double result) {
+        assert entry != null;
+        inputList.remove(entry.getKey() + 1);
+        inputList.remove(entry.getKey().intValue());
+        inputList.remove(entry.getKey() - 1);
+        inputList.add(entry.getKey() - 1, String.valueOf(result));
+        return operationFinder(inputList.toArray());
     }
 
     public static double sum(double x, double y) {
